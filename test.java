3cc0f8d0f0cf708855730b297ae43cfdd252ad49
aditya <aditya.aa937@gmail.com>
2020-02-07 11:46:08 +0530

    update test.java

--- test.java
+++ test.java
@@ -2,7 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");
      System.out.println("Hello Febuu,How are you??????");
-     System.out.println("Hello Java");
      System.out.println("Hello Baby!! How do you dooo");
     }  
 } 
